d6fabedce29a30ea44f6b19e3ba672b9b84b9534
adityabhilare <147392304+adityabhilare@users.noreply.github.com>
2025-01-26 18:56:06 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "aditya@gmail.com"
+var accountPassword = "Wai"
+accountCity =  "Pune"
+
+accountId = 2
+
+console.log(accountId);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"Use strict"; // navin syntax nusar file run karnyasathichi line ahe pn ata cha time la JS advance zala ahe to autometically sagle navin syntax ani running methode nusar run karto.
+
+// alert(3+3) // we are using NodeJS not browser.. browser madhe alert dakhvnar pn NodeJs madhe nahi dakhvu shkt karan JS cha engine browser madhech asta tithe possible ahe.
+
+// Code redability nehmi changli asavi.. vyavasthith pretier code asava.
+
+console.log( 3 + 3 );
+console.log("aditya");
+
+
+let name = "Aditya"
+let age = 22
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof " aditya ")
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
