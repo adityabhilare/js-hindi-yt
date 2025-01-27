9753942083a05315ec968c161cf9452ce3f28522
adityabhilare <147392304+adityabhilare@users.noreply.github.com>
2025-01-27 07:03:04 +0000

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,31 @@
+//************
+// // premitive datatypes
+// there are 7 premitive types-- 
+// 1.String 2.Number 3.Boolean 4.null 5.undefined 6.symbol 7.BigInt
+
+// javascript is a dynamic type. 
+
+
+// Referance Datatypes (Non Primitive)
+// array, Objects, Functions
+
+const heros = ["shaktiman", "Naggraj", "Doga"]
+let myObj={
+    name: "aditya",
+    age :22,
+
+}
+
+const myFunction = function(){
+    console.log("Hello World!");
+}
+
+bigNum = 2222255548n
+console.log(typeof bigNum); // bigint
+
+// Interview sathi Q - typeof value varun tyachi datatype kay ahe te sangaycha.
+// jasa ki null -- object
+// boolean -- Boolean
+//  Number -- number, etc. search karne.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof#typeof_null
+
