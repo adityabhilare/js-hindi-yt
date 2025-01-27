a6795410c932cb662afd7b20f21a2d65689d3f0f
adityabhilare <147392304+adityabhilare@users.noreply.github.com>
2025-01-27 06:06:24 +0000

    Conversion of Datatypes.

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score =null
+
+console.log(typeof score);
+
+
+let valInNumber = Number (score)
+console.log(typeof valInNumber);
+console.log(valInNumber);
+
+// apan number la convert karu shakto pn jar 33abc asa asel tr NaN output yeanra.
+//True -- 1,,, false -- 0
+
+
+let isloggedIn = 0
+let booleanIsloggedIn = Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
+// false
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+// conversion in string.
+
+
+
