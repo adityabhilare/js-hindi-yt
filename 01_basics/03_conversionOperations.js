1fdd8994d4465e85c0846d1de4c753d6668d8fe7
adityabhilare <147392304+adityabhilare@users.noreply.github.com>
2025-01-27 06:24:25 +0000

    operations addition

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -25,3 +25,47 @@ console.log(typeof stringNumber);
 
 
 
+
+// **************** OPERATIONS ************************
+
+
+let value = 3
+let negVal = -value
+console.log(negVal);
+// making negative value.
+
+console.log(2+2); // 4
+console.log(2-2); // 0
+console.log(2*2); // 4
+console.log(2**3); // 8 power
+console.log(2%3); // 2 reminder
+console.log(2/2); // 1 bhagakar
+
+let str1 = "Aditya"
+let str2 = "Bhilare"
+let str3 = str1+str2 // adding string 
+console.log(str3);
+
+console.log("1"+2); // 12
+console.log(1 + "2"); // 12
+console.log("1"+"3"); // 13
+console.log("1"+2+2); //122 jar adhi string asel tr total operation la string madhe convert kele jate.
+console.log(1 + 2 +"2"); // 32  jar string last la asel tr adhi operation solve honar je asel te nantr dileli string last la add honar.
+
+
+let gameCounter = 100
+gameCounter++; // 101 postIncrement for better knowledge  refer MDN incremental operator note.
+console.log(gameCounter);
+
+l
+
+
+
+
+
+
+
+
+
+
+
