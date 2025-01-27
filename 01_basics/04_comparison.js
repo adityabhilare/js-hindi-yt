6b8eb4437e2cb6b413746c3f54f820555b9c9476
adityabhilare <147392304+adityabhilare@users.noreply.github.com>
2025-01-27 06:41:00 +0000

    comparison operations

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,41 @@
+console.log(2>1); // true
+console.log(2>=1); // true
+console.log(2< 1); // false
+console.log(2 == 1); // false
+console.log(2!=1); // true
+
+// ****************************
+console.log("Next string shi comparison");
+console.log("2">1); // true
+console.log("02" > 1); //true
+
+// *******************************
+
+console.log("Null chi Bhangad");
+console.log(null > 0); // false yachamdhe comparison operator cha veles null la 0 assign kele jate so false asach >= sathi pn aste pn == sathi naste.
+console.log(null == 0); // fa;se
+console.log(null >= 0); // true
+
+console.log("Undefined cha vishay");
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+
+// *********************************
+
+console.log("==  ani  === cha matter");
+
+console.log("2" == 2); // true
+console.log( "2 "=== 2); //  false
+// yachat == asel tr comparison operator yeto ani === asel tr adhi datatype compare hoto yachat adhi string ahe ani nantr number so ha false ahe.
+
+
+
+
+
+
+
+
+
+
